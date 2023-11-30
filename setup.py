ec50d48aba79ffd836e780ec7839b4df52beb63e
InquestGeronimo <rcosta84@gmail.com>
2023-11-30 18:42:51 -0500

    edit setup

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 VERSION = "0.1.3"
 DESCRIPTION = "Using Gladia's Whisper API for transcribing YouTube videos"
 LONG_DESCRIPTION = "Use Whisper speech-to-text to transcribe YouTube videos and run postprocessing with the transcription."
-DEPENDENCIES = ["requests>=2.31.0", "pandas>=2.1.3", "yt_dlp>=2023.11.16, streamlink>=6.4.2"]
-# Setting up
+DEPENDENCIES = ["requests>=2.31.0", "pandas>=2.1.3", "yt_dlp>=2023.11.16", "streamlink>=6.4.2"]
+
 setup(
     name="whisperyt",
     version=VERSION,
@@ -19,10 +19,10 @@ setup(
     author_email="rcostanl@gmail.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     package_dir={"": "src"},
     packages=find_packages("src"),
-    install_requires=,
+    install_requires=DEPENDENCIES,
     keywords=["python", "youtube", "whisper", "transcription", "speech-to-text", "ASR"],
     classifiers=[
         "Development Status :: 1 - Planning",
